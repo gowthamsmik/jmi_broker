c3c9029d077791de7ee325612281d2d36a3a54f0
gowthamsmik <gowtham.smiksystems@gmail.com>
2023-11-18 18:28:53 +0530

    jmi broker

--- control-panel/account-overview.php
+++ control-panel/account-overview.php
@@ -1,59 +1,72 @@
 <html>
 
 <head>
-<?php include("includes/style.php"); ?>
-<style>
-    .body{
-        background-color: #F0F0;
-    }
-    .border-bottom-80 {
-    border-bottom: 1px solid #dee2e6; /* or your preferred border color */
-    height: 80%;
-}
-</style>
+    <?php include("includes/style.php"); ?>
+    <style>
+        .body {
+            background-color: #F0F0F0;
+        }
+
+        .border-bottom-80 {
+            border-bottom: 1px solid #dee2e6;
+            /* or your preferred border color */
+            height: 80%;
+        }
+        .card {
+            border-radius: 10px;
+        }
+        .account_circle{
+            height: 30px;
+            width: 30px;
+        }
+    </style>
 </head>
+
 <body>
 
     <div>
-        <h2>Control Panel|Account OverView</h2>
-        <div class="card p-3 bg-white rounded-full my-3 py-5">
-            <div class= "border-bottom-80 p-1 d-flex justify-content-between">
+        <div class="d-flex">
+            <h2>Control Panel|Account OverView</h2>
+            <div class="d-flex ml-auto"><img src="assets/images/svg/account_circle.svg" class="account_circle"  alt=""> <p class="mt-1 ms-2">Welcome, Ameer Ameer</p></div>
+        </div>
+        <div class="card p-3 bg-white my-3 py-5">
+            <div class="border-bottom-80 p-1 d-flex justify-content-between">
                 <h3>FOREX ACCOUNTS</h3>
                 <h3 class="ml-auto">28452.03 USD</h3>
             </div>
             <div class="mt-3 p-1 d-flex justify-content-between">
-            <h3>TOTAL VALUE</h3>
-            <h3 class="ml-auto">28452.03 USD</h3>
+                <h3>TOTAL VALUE</h3>
+                <h3 class="ml-auto">28452.03 USD</h3>
             </div>
         </div>
-        <div class="card p-3 bg-white rounded-full my-3 py-3">
+        <div class="card p-3 bg-white my-3 py-3">
             <div class="mt-2 p-1 d-flex ">
-            <img src="assets/images/logo.svg" alt="Link 1 Icon" height="40px" width="40px">
-            <div class="mx-5 w-50">
-                <h5>Ameer</h5>
-                <p>FOREX 9190</p>
-            </div>
-            <h3 class="text-end">28452.03 USD</h3>
+                <img src="assets/images/logo.svg" alt="Link 1 Icon" height="40px" width="40px">
+                <div class="mx-5 w-50">
+                    <p class="font-weight-bold">Ameer</p>
+                    <p>FOREX 9190</p>
+                </div>
+                <h3 class="text-end">28452.03 USD</h3>
             </div>
         </div>
-        <div class="card p-3 bg-white rounded-full my-3 py-3">
+        <div class="card p-3 bg-white my-3 py-3">
             <div class="mt-2 p-1 d-flex ">
-            <img src="assets/images/logo.svg" alt="Link 1 Icon" height="40px" width="40px">
-            <div class="mx-5 w-50">
-                <h5>Ameer</h5>
-                <p>FOREX 9190</p>
-            </div>
-            <h3 class="text-end">28452.03 USD</h3>
+                <img src="assets/images/logo.svg" alt="Link 1 Icon" height="40px" width="40px">
+                <div class="mx-5 w-50">
+                    <h5>Ameer</h5>
+                    <p>FOREX 9190</p>
+                </div>
+                <h3 class="text-end">28452.03 USD</h3>
             </div>
         </div>
-        <div class="card p-3 bg-white rounded-full my-3 py-3">
+        <div class="card p-3 bg-white my-3 py-3">
             <div class="mt-2 p-1 d-flex ">
-            <img src="assets/images/logo.svg" alt="Link 1 Icon" height="40px" width="40px">
-            <div class="mx-5 w-50">
-                <h5>Ameer</h5>
-                <p>FOREX 9190</p>
-            </div>
-            <h3 class="text-end">28452.03 USD</h3>
+                <img src="assets/images/logo.svg" alt="Link 1 Icon" height="40px" width="40px">
+                <div class="mx-5 w-50">
+                    <h5>Ameer</h5>
+                    <p>FOREX 9190</p>
+                </div>
+                <h3 class="text-end">28452.03 USD</h3>
             </div>
         </div>
     </div>
--- user-website.php
+++ user-website.php
@@ -4,10 +4,11 @@
 <head>
    <?php include("includes/compatibility.php"); ?>
    <meta name="description" content="">
+   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
    <title>Dashboard</title>
    <?php include("includes/style.php"); ?>
    <style>
-      .dasbord-subheader-bg {
+      .dashboard-subheader-bg {
          height: 130px;
          width: 100%;
          background: url(assets/images/svg/Gradient.png);
@@ -15,12 +16,11 @@
          overflow: hidden;
       }
 
-      .dasbord-subheader-box {
+      .dashboard-subheader-box {
          height: 232px;
          width: auto;
          background: url(assets/images/svg/2.svg);
          background-size: 100% 100%;
-
       }
 
       .position {
@@ -36,11 +36,12 @@
       .layout {
          display: flex;
          width: 100%;
+         background-color: #F0F0F0;
       }
 
       .sidebar {
          width: 30%;
-         background-color: #FED801;
+         background-color: #ffbf10;
          /* padding-top: 20px; */
          margin: 10px;
          border-radius: 10px;
@@ -58,30 +59,49 @@
       }
 
       .sidebar a:hover {
-
-         background-color: #0059B2;
+         color: white;
+         background-color: #0342ab;
       }
 
       .sidebar a:active {
-
-         background-color: #0059B2;
+         color: white;
+         background-color: #0342ab;
       }
 
       .content {
          flex: 1;
          padding: 16px;
-         border: 2px solid green;
          margin: 10px;
       }
 
-      
+      .nav-tabs .nav-item .nav-link {
+         color: white;
+         border-radius: 0px;
+      }
+
+      .nav-tabs .nav-item .nav-link:hover {
+         color: white;
+         border-color: #0342ab;
+         border-radius: 0px;
+      }
+
+      .nav-tabs .nav-item .nav-link.active {
+         background-color: #0342ab;
+         color: white;
+         border-color: #0342ab;
+         border-radius: 0px;
+      }
+
+      #myTabs{
+         margin-bottom: -8px;
+      }
    </style>
 </head>
 
 <body>
    <?php include("includes/header.php"); ?>
-   <div class="dasbord-subheader-bg">
-      <div class="dasbord-subheader-box">
+   <div class="dashboard-subheader-bg">
+      <div class="dashboard-subheader-box">
          <div class="position">
             <h5 class="text-white">Control Panel | Account Overview</h5>
             <p class="text-white font-size">Home > Dashboard</p>
@@ -92,59 +112,54 @@
 
       <div class="sidebar">
          <ul class="nav nav-tabs border-0 row" id="myTabs">
-            <li class="nav-item col">
-               <a class="nav-link active py-2 px-0" id="tab1-tab" data-toggle="tab" href="#" onclick="showRoute('link13')">
+            <li class="nav-item col p-0 ">
+               <a class="nav-link active m-0 w-100 h-100 d-flex" id="tab1-tab" data-toggle="tab" href="#" data-target="#tab1" onclick="showRoute('link13')">
                   <img src="assets/images/svg/hamburger.svg" alt="Link 1 Icon">
-                  My Account</a>
+                  <p class="text-white ms-2">My Account</p></a>
             </li>
-            <li class="nav-item col">
-               <a class="nav-link py-2 px-0" id="tab2-tab" data-toggle="tab" href="#" onclick="showRoute('link14')"> <img
+            <li class="nav-item col p-0">
+               <a class="nav-link m-0 w-100 h-100 d-flex" id="tab2-tab" data-toggle="tab" href="#" data-target="#tab2" onclick="showRoute('link14')"> <img
                      src="assets/images/svg/settings.svg" alt="Link 1 Icon">
-                  Tools</a>
+                  <p class="text-white ms-2">Tools</p></a>
             </li>
          </ul>
          <div class="tab-content mt-2">
             <div class="tab-pane fade show active" id="tab1">
-               <a class="active" href="#" onclick="showRoute('link1')"> <img src="assets/images/sidebar/image9.svg"
+               <a class="d-flex" href="#" onclick="showRoute('link1')"> <img src="assets/images/sidebar/image9.svg"
                      alt="Link 1 Icon">
-                  Account Overview</a>
-               <a href="#" onclick="showRoute('link2')"> <img src="assets/images/sidebar/image12.svg"
-                     alt="Link 1 Icon">Open
-                  Live Account</a>
-               <a href="#" onclick="showRoute('link3')"> <img src="assets/images/sidebar/image6.svg" alt="Link 1 Icon">
-                  Open
-                  Demo Account</a>
-               <a href="#" onclick="showRoute('link4')"> <img src="assets/images/sidebar/image1.svg"
-                     alt="Link 1 Icon">Add
-                  Existing Account</a>
+                  <h4 class="text-white ms-3 mt-2">Account Overview</h4></a>
+               <a class="d-flex" href="#" onclick="showRoute('link2')"> <img src="assets/images/sidebar/image12.svg"
+                     alt="Link 1 Icon"> <h4 class="text-white ms-3 mt-2">Open Live Account</h4></a>
+               <a class="d-flex" href="#" onclick="showRoute('link3')"> <img src="assets/images/sidebar/image6.svg" alt="Link 1 Icon">
+               <h4 class="text-white ms-3 mt-2">Open Demo Account</h4></a>
+               <a class="d-flex" href="#" onclick="showRoute('link4')"> <img src="assets/images/sidebar/image1.svg"
+                     alt="Link 1 Icon"><h4 class="text-white ms-3 mt-2">Add Existing Account</h4></a>
 
-               <a href="#" onclick="showRoute('link5')"> <img src="assets/images/sidebar/image11.svg"
-                     alt="Link 1 Icon">Live
-                  Accounts</a>
+               <a class="d-flex" href="#" onclick="showRoute('link5')"> <img src="assets/images/sidebar/image11.svg"
+                     alt="Link 1 Icon"><h4 class="text-white ms-3 mt-2">Live Accounts</h4></a>
 
-               <a href="#" onclick="showRoute('link6')"> <img src="assets/images/sidebar/image3.svg" alt="Link 1 Icon">
-                  Deposit</a>
+               <a class="d-flex" href="#" onclick="showRoute('link6')"> <img src="assets/images/sidebar/image3.svg" alt="Link 1 Icon">
+               <h4 class="text-white ms-3 ps-1 mt-2">Deposit</h4></a>
 
-               <a href="#" onclick="showRoute('link7')"> <img src="assets/images/sidebar/image4.svg" alt="Link 1 Icon">
-                  Withdraw</a>
+               <a class="d-flex" href="#" onclick="showRoute('link7')"> <img src="assets/images/sidebar/image4.svg" alt="Link 1 Icon">
+               <h4 class="text-white ms-3 ps-1 mt-2">Withdraw</h4></a>
 
-               <a href="#" onclick="showRoute('link8')"> <img src="assets/images/sidebar/image10.svg"
-                     alt="Link 1 Icon">Internal Transfers</a>
+               <a class="d-flex" href="#" onclick="showRoute('link8')"> <img src="assets/images/sidebar/image10.svg"
+                     alt="Link 1 Icon"><h4 class="text-white ms-3 mt-2">Internal Transfers</h4></a>
 
-               <a href="#" onclick="showRoute('link9')"> <img src="assets/images/sidebar/image8.svg"
-                     alt="Link 1 Icon">Copy
-                  Trade</a>
+               <a class="d-flex" href="#" onclick="showRoute('link9')"> <img src="assets/images/sidebar/image8.svg"
+                     alt="Link 1 Icon" class="my-1"><h4 class="text-white ms-3 ps-1 mt-2">Copy Trade</h4></a>
 
-               <a href="#" onclick="showRoute('link10')"> <img src="assets/images/sidebar/image10.svg"
-                     alt="Link 1 Icon">Transaction History</a>
-               <a href="#" onclick="showRoute('link11')"> <img src="assets/images/sidebar/image2.svg"
-                     alt="Link 1 Icon">Referral System</a>
-               <a href="#" onclick="showRoute('link12')"> <img src="assets/images/sidebar/image7.svg"
-                     alt="Link 1 Icon">My
-                  Referrals</a>
+               <a class="d-flex" href="#" onclick="showRoute('link10')"> <img src="assets/images/sidebar/image10.svg"
+                     alt="Link 1 Icon"><h4 class="text-white ms-2 ps-1 mt-2">Transaction History</h4></a>
+               <a class="d-flex" href="#" onclick="showRoute('link11')"> <img src="assets/images/sidebar/image2.svg"
+                     alt="Link 1 Icon" class="my-1"><h4 class="text-white ms-3 ps-1 mt-2">Referral System</h4></a>
+               <a class="d-flex" href="#" onclick="showRoute('link12')"> <img src="assets/images/sidebar/image7.svg"
+                     alt="Link 1 Icon"><h4 class="text-white ms-3 ps-1 mt-2">My Referrals</h4></a>
                <!-- Add more links as needed -->
             </div>
             <div class="tab-pane fade" id="tab2">
+               <h2>jj</h2>
             </div>
          </div>
 
