bd888dbf10a1d79638bfbd5b2f05ace65eba96a6
rlmmuthukumar <rlm.muthukumar@gmail.com>
2023-11-27 19:45:22 +0530

    changes made

--- user-website.php
+++ user-website.php
@@ -79,6 +79,11 @@
          background-color: #0342ab;
       }
 
+      .sidebar .tab-pane .active{
+         color: white;
+         background-color: #0342ab; 
+      }
+
       .content {
          flex: 1;
          padding: 1%;
@@ -189,6 +194,7 @@
    <div class='layout'>
       <div class="sidebar-box">
          <div class="sidebar">
+            <input type="hidden" value="" id="paneselected" />
             <ul class="nav nav-tabs border-0 row px-2" id="myTabs">
                <li class="nav-item col p-0 ">
                   <a class="nav-link active m-0 w-100 h-100 d-flex px-2" id="tab1-tab" data-toggle="tab" href="#"
@@ -208,57 +214,57 @@
             </ul>
             <div class="tab-content mt-2">
                <div class="tab-pane fade show active" id="tab1">
-                  <a class="d-flex" href="#" onclick="showRoute('link1')"> <img src="assets/images/sidebar/image9.svg"
+                  <a class="d-flex" href="#" onclick="showRoute('link1')" id="mylink1"> <img src="assets/images/sidebar/image9.svg"
                         alt="Link 1 Icon">
                      <h4 class="text-white ms-3 mt-2 my_account">Account Overview</h4>
                   </a>
-                  <a class="d-flex" href="#" onclick="showRoute('link2')"> <img src="assets/images/sidebar/image12.svg"
+                  <a class="d-flex" href="#" onclick="showRoute('link2')" id="mylink2"> <img src="assets/images/sidebar/image12.svg"
                         alt="Link 1 Icon">
                      <h4 class="text-white ms-3 mt-2 my_account">Open Live Account</h4>
                   </a>
-                  <a class="d-flex" href="#" onclick="showRoute('link3')"> <img src="assets/images/sidebar/image6.svg"
+                  <a class="d-flex" href="#" onclick="showRoute('link3')" id="mylink3"> <img src="assets/images/sidebar/image6.svg"
                         alt="Link 1 Icon">
                      <h4 class="text-white ms-3 mt-2 my_account">Open Demo Account</h4>
                   </a>
-                  <a class="d-flex" href="#" onclick="showRoute('link4')"> <img src="assets/images/sidebar/image1.svg"
+                  <a class="d-flex" href="#" onclick="showRoute('link4')" id="mylink4"> <img src="assets/images/sidebar/image1.svg"
                         alt="Link 1 Icon">
                      <h4 class="text-white ms-3 mt-2 my_account">Add Existing Account</h4>
                   </a>
 
-                  <a class="d-flex" href="#" onclick="showRoute('link5')"> <img src="assets/images/sidebar/image11.svg"
+                  <a class="d-flex" href="#" onclick="showRoute('link5')" id="mylink5"> <img src="assets/images/sidebar/image11.svg"
                         alt="Link 1 Icon">
                      <h4 class="text-white ms-3 mt-2 my_account">Live Accounts</h4>
                   </a>
 
-                  <a class="d-flex" href="#" onclick="showRoute('link6')"> <img src="assets/images/sidebar/image3.svg"
+                  <a class="d-flex" href="#" onclick="showRoute('link6')" id="mylink6"> <img src="assets/images/sidebar/image3.svg"
                         alt="Link 1 Icon">
                      <h4 class="text-white ms-3 ps-1 mt-2 my_account">Deposit</h4>
                   </a>
 
-                  <a class="d-flex" href="#" onclick="showRoute('link7')"> <img src="assets/images/sidebar/image4.svg"
+                  <a class="d-flex" href="#" onclick="showRoute('link7')" id="mylink7"> <img src="assets/images/sidebar/image4.svg"
                         alt="Link 1 Icon">
                      <h4 class="text-white ms-3 ps-1 mt-2 my_account">Withdraw</h4>
                   </a>
 
-                  <a class="d-flex" href="#" onclick="showRoute('link8')"> <img src="assets/images/sidebar/image10.svg"
+                  <a class="d-flex" href="#" onclick="showRoute('link8')" id="mylink8"> <img src="assets/images/sidebar/image10.svg"
                         alt="Link 1 Icon">
                      <h4 class="text-white ms-3 mt-2 my_account">Internal Transfers</h4>
                   </a>
 
-                  <a class="d-flex" href="#" onclick="showRoute('link9')"> <img src="assets/images/sidebar/image8.svg"
+                  <a class="d-flex" href="#" onclick="showRoute('link9')" id="mylink9"> <img src="assets/images/sidebar/image8.svg"
                         alt="Link 1 Icon" class="my-1">
                      <h4 class="text-white ms-3 ps-1 mt-2 my_account">Copy Trade</h4>
                   </a>
 
-                  <a class="d-flex" href="#" onclick="showRoute('link10')"> <img src="assets/images/sidebar/image10.svg"
+                  <a class="d-flex" href="#" onclick="showRoute('link10')" id="mylink10"> <img src="assets/images/sidebar/image10.svg"
                         alt="Link 1 Icon">
                      <h4 class="text-white ms-2 ps-1 mt-2 my_account">Transaction History</h4>
                   </a>
-                  <a class="d-flex" href="#" onclick="showRoute('link11')"> <img src="assets/images/sidebar/image2.svg"
+                  <a class="d-flex" href="#" onclick="showRoute('link11')" id="mylink11"> <img src="assets/images/sidebar/image2.svg"
                         alt="Link 1 Icon" class="my-1">
                      <h4 class="text-white ms-3 ps-1 mt-2 my_account">Referral System</h4>
                   </a>
-                  <a class="d-flex" href="#" onclick="showRoute('link12')"> <img src="assets/images/sidebar/image7.svg"
+                  <a class="d-flex" href="#" onclick="showRoute('link12')" id="mylink12"> <img src="assets/images/sidebar/image7.svg"
                         alt="Link 1 Icon">
                      <h4 class="text-white ms-3 ps-1 mt-2 my_account">My Referrals</h4>
                   </a>
@@ -397,10 +403,15 @@
 
       // Show the selected route content
       var selectedRoute = document.getElementById(routeId);
+      var highlighted = document.getElementById('my'+routeId);
+      var deselectpane = document.getElementById('paneselected').value; 
       if (selectedRoute) {
          selectedRoute.style.display = 'block';
+         highlighted.className = 'de-flex active';
+         if(deselectpane !=="")
+         document.getElementById('my'+deselectpane).className = "de-flex";
       }
-
+      document.getElementById('paneselected').value = routeId;
       var routeText = document.getElementById('routeText');
       if (routeText) {
          switch (routeId) {
